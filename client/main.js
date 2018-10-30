64e2cb74dffadc61b2770dbabf3583acac6b0492
Frank Eduardo Araque Cadenas <Frank Eduardo Araque Cadenas>
2018-10-30 23:38:39 +0100

    Add repository

--- client/main.js
+++ client/main.js
@@ -1,42 +1,136 @@
 Template.availableTimes.onCreated(function() {
   this.timeSlots = new ReactiveVar();
   this.selectedDay = new ReactiveVar();
-})
-
-
+  this.selectedHour = new ReactiveVar(1);
+});
 
 Template.availableTimes.onRendered(function() {
   this.autorun(() => {
-    this.subscribe('bookings', this.selectedDay.get(), () => {
-      const busyTimes = Bookings.find().fetch();
-      // show all available timeslots for a given input (week)
-      // assume work hours 8-16h including weekends, each time slot starts at full hour
-      // booking can be max 8 hours long and cannot span across multiple days
-      // the result array is to be assigned to a reactive variable called "timeSlots"
-      // the html template is currently set to display the values as a text, but feel free to change it
-      // if you want to keep it as text, it could look like this: ["8 - 10", "9 - 11", ...]
-      // ... your code goes here
+    this.subscribe(
+      'bookings',
+      this.selectedDay.get(),
+      this.selectedHour.get(),
 
-    })
-  })
-})
+      () => {
+        // show all available timeslots for a given input (week)
+        // assume work hours 8-16h including , each time slot starts at full hour
+        // booking can be max 8 hours long and cannot span across multiple days
+        // the result array is to be assigned to a reactive variable called "timeSlots"
+        // the html template is currently set to display the values as a text, but feel free to change it
+        // if you want to keep it as text, it could look like this: ["8 - 10", "9 - 11", ...]
+        // ... your code goes here
 
+        // This function Extract the start time from the string [e.g "10-11" then return 10]
+        const getStartTime = time => {
+          let split = time.split('-');
+          return split[0];
+        };
+        //Get all the existing bookings from the db collection filtered by the selected day.
+        const busyTimes = Bookings.find({
+          day: this.selectedDay.get()
+        }).fetch();
 
+        const availableTime = () => {
+          // By default the AvailableHours array contains all available slot time, from 8:00 till 16:00. This function
+          // remove from the array those slots time that have been already booked.
+          //{    day: this.selectedDay.get()}
+
+          function removeTimeSlot() {
+            busyTimes.forEach(busytime => {
+              availableHours.map((time, i) => {
+                if (getStartTime(time) == busytime.from) {
+                  availableHours.splice(i, busytime.hours);
+                }
+              });
+              return availableHours;
+            });
+          }
+          function chunkArray(availableHours, chunkSize) {
+            let chunks = [],
+              i = 0,
+              n = availableHours.length;
+            while (i < n) {
+              chunks.push(availableHours.slice(i, (i += chunkSize)));
+            }
+
+            return chunks;
+          }
+
+          let availableHours = [
+            '8-9',
+            '9-10',
+            '10-11',
+            '11-12',
+            '12-13',
+            '13-14',
+            '14-15',
+            '15-16'
+          ];
+
+          removeTimeSlot();
+          let requiredHours = this.selectedHour.get();
+          let totalHoursAvailable = availableHours.length;
+
+          if (requiredHours > totalHoursAvailable) {
+            return [
+              `There is/are not ${requiredHours} hr/hrs available on (${this.selectedDay.get()})`
+            ];
+          } else {
+            return chunkArray(availableHours, requiredHours);
+          }
+
+          //return availableHours;
+        };
+
+        this.timeSlots.set(availableTime());
+      }
+    );
+  });
+});
 
 Template.availableTimes.helpers({
   days() {
     // return an array of following 5 working days (Mon-Fri) incl. today
-    // ... your code goes here
-    // hardcoded example:
-    return ['2018-10-26', '2018-10-29', '2018-10-30', '2018-10-31', '2018-11-01']
+    // return true if the current date(d) is within a weekend
+    let isItWeekEnd = d => {
+      if (d.getDay() == 0 || d.getDay() == 6) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    //return an array with next five working days since todays's date
+    let findWeekDays = () => {
+      let nextFiveWorkingDays = [],
+        date = new Date(),
+        i = 0;
+
+      do {
+        let curdate = new Date(
+          date.getFullYear(),
+          date.getMonth(),
+          date.getDate() + i
+        );
+        if (!isItWeekEnd(curdate)) {
+          nextFiveWorkingDays.push(curdate);
+        }
+        i++;
+      } while (nextFiveWorkingDays.length < 5);
+      return nextFiveWorkingDays;
+    };
+
+    return findWeekDays();
   },
 
   hours: [1, 2, 3, 4, 5, 6, 7, 8],
 
   timeSlots() {
-    return Template.instance().timeSlots.get()
+    return Template.instance().timeSlots.get();
   },
 
+  selectedHour() {
+    return Template.instance().selectedHour.get();
+  },
   bookings() {
     return Bookings.find().fetch();
   },
@@ -47,10 +141,8 @@ Template.availableTimes.helpers({
 
   selectedDay() {
     return Template.instance().selectedDay.get();
-  },
+  }
 });
-
-
 
 Template.availableTimes.events({
   'click #findTimeSlots'(e, t) {
@@ -58,9 +150,25 @@ Template.availableTimes.events({
   },
 
   'click .timeSlotSelect'(e, t) {
+    let array = e.currentTarget.text.split('-');
+    let from = array[0];
+    let to = array[array.length - 1];
+    let day = t.selectedDay.get();
+    let hours = Number(to) - Number(from);
+
+    t.selectedDay.set(0);
+    t.selectedDay.set($('#selectedDay').val());
+    // t.selectedHour.set(0);
+    // t.selectedHour.set($('#hourCount').val());
+    //
+
     e.preventDefault();
     // finish the application by calling a server method to book the time slot
-    // Meteor.call('bookTimeSlot', {day, from, to})
+    Meteor.call('bookTimeSlot', { day, from, to, hours });
     // ... your code goes here
   },
+  'change #hourCount': function(e, t) {
+    // var category = $(e.currentTarget).val();
+    t.selectedHour.set($(e.currentTarget).val());
+  }
 });
--- server/main.js
+++ server/main.js
@@ -2,21 +2,23 @@ Meteor.methods({
   bookTimeSlot(requestedTimeSlot) {
     // finish the time slot reservation function
     // .. your code goes here
-    throw new Meteor.Error('this is not defined yet')
+    Bookings.insert({
+      day: requestedTimeSlot.day,
+      from: requestedTimeSlot.from,
+      to: requestedTimeSlot.to,
+      hours: requestedTimeSlot.hours
+    });
+    //throw new Meteor.Error('this is not defined yet');
   }
-})
-
-
+});
 
 Meteor.publish('bookings', function(day) {
-  return Bookings.find({day: day})
-})
-
-
+  return Bookings.find({ day: day });
+});
 
 Meteor.startup(() => {
   if (Bookings.find().count() === 0) {
-    Bookings.insert({day: '2018-10-29', from: 8, to: 10, hours: 2});
-    Bookings.insert({day: '2018-10-29', from: 13, to: 14, hours: 1});
+    Bookings.insert({ day: '2018-10-29', from: 8, to: 10, hours: 2 });
+    Bookings.insert({ day: '2018-10-29', from: 13, to: 14, hours: 1 });
   }
 });
